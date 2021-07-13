f92a0b391cbaf480b59dc23c508bfbace1f23992
menna <menna.mohamed.omar@gmail.com>
2021-07-13 21:02:32 +0200

    remove image from response

--- model/category.js
+++ model/category.js
@@ -18,7 +18,7 @@ schema.methods.toJSON = function () {
   const category = this;
   const categoryObject = category.toObject();
   delete categoryObject.__v;
-  // delete categoryObject.categoryImage;
+  delete categoryObject.categoryImage;
   return categoryObject;
 };
 
